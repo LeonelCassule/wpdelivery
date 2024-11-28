4f0a5f6752849447204becb85b1723ac084765d8
LeonelCassule <leonelcassule1994@gmail.com>
2024-11-28 23:38:12 +0100

    Finalizado

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1733245751;s:7:"authKey";s:64:"7S0-XD4JuPqX-+f(]a8@hy`9mbP~P&{9o/=azyDHUQ-,Oo/h uu7z<Y[v8BNIW`(";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1607;}
